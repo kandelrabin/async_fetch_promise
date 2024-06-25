d0f95db883a73b7f2089a0a8a637fbbf27c185c8
Rabin Kandel <kandelrabin@live.com>
2024-06-25 11:12:46 +0100

    add async method fetchDogImage to get multiple images

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-const fetchDogImage = async () => {
-    const response = await fetch("https://dog.ceo/api/breeds/image/random");
-    const jsonData = await response.json();
-    document.querySelector("#dog-image").src = jsonData.message;
-}
-
-// // CALLBACK HELL
+// // Old way of doing fetch: CALLBACK HELL
 // const fetchDogImage = () => {
 //     fetch("https://dog.ceo/api/breeds/image/random")
 //         .then((response) => response.json())  
 //         .then(data => document.querySelector("#dog-image").src = data.message)
 // }
+
+// // Fetching single image
+// const fetchDogImage = async () => {
+//     const response = await fetch("https://dog.ceo/api/breeds/image/random");
+//     const jsonData = await response.json();
+//     document.querySelector("#dog-image").src = jsonData.message;
+// }
+
+// Fetching miltiple images
+
+const fetchDogImage = async () => {
+    const breedResponse = await fetch("https://dog.ceo/api/breed/dachshund/images");
+    const breedJson = await breedResponse.json();
+
+    const imagesContainer = document.createElement("div");
+    breedJson.message.forEach((url) => {
+        const dogImage = document.createElement("img");
+        dogImage.src = url;
+        imagesContainer.appendChild(dogImage);
+    });
+    document.querySelector("body").appendChild("imagesContainer");
+}
+
+
 
 
 document.querySelector("#dog-button").addEventListener("click", fetchDogImage);
